468d13740c19024239f6fdea51de036d0f82a9b8
Tilo Kowalski <dev@tilokowalski.de>
2024-02-02 11:50:39 +0100

    Add Hello World to main

--- src/main/java/dxc/edu/DxcRakete.java
+++ src/main/java/dxc/edu/DxcRakete.java
@@ -3,6 +3,7 @@ package dxc.edu;
 public class DxcRakete {
 
     public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 
     public static int multiply(int numberOne, int numberTwo) {
