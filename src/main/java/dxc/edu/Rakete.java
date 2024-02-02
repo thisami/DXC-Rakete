a64495fae1e53e000242b17f20a80c2942a1370f
Tilo Kowalski <dev@tilokowalski.de>
2024-02-02 14:03:07 +0100

    refactor rakete and rakete test

--- src/main/java/dxc/edu/Rakete.java
+++ src/main/java/dxc/edu/Rakete.java
@@ -2,26 +2,32 @@ package dxc.edu;
 
 public class Rakete {
 
-
     private double treibstoffZustand;
+
     private String farbe;
+
     private String name;
+
     private int personenKapazitaet;
+
     private String hersteller;
+
     private boolean nuklearAntrieb = false;
+
     private int geschwindigkeit = 0;
+
     private boolean motorenLaufen = false;
 
-    public void  motorenStarten()throws IllegalStateException{
-        if (motorenLaufen){
+    public void motorenStarten() throws IllegalStateException {
+        if (motorenLaufen) {
             throw new IllegalStateException("Motoren laufen bereits");
-        }
-        else {
+        } else {
             motorenLaufen = true;
         }
     }
 
-    public boolean getMotorenLaufen(){
+    public boolean isMotorenLaufen() {
         return motorenLaufen;
     }
+
 }
--- src/test/java/dxc/edu/RaketeTest.java
+++ src/test/java/dxc/edu/RaketeTest.java
@@ -1,16 +1,20 @@
 package dxc.edu;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RaketeTest {
 
     @Test
-    void testMotorenStarten(){
+    void testMotorenStarten() {
         Rakete rakete = new Rakete();
         assertFalse(rakete.getMotorenLaufen());
+
         rakete.motorenStarten();
         assertTrue(rakete.getMotorenLaufen());
-        assertThrows(IllegalStateException.class, ()->rakete.motorenStarten());
+
+        assertThrows(IllegalStateException.class, () -> rakete.motorenStarten());
     }
+
 }
