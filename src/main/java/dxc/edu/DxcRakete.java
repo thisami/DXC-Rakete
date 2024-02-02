af29087b8ac2c96a84b16dba62c579c8c4e014e4
Tilo Kowalski <dev@tilokowalski.de>
2024-02-02 13:50:58 +0100

    remove boilerplate code

--- src/main/java/dxc/edu/DxcRakete.java
+++ src/main/java/dxc/edu/DxcRakete.java
@@ -3,15 +3,6 @@ package dxc.edu;
 public class DxcRakete {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-
-    public static int multiply(int numberOne, int numberTwo) {
-        return numberOne * numberTwo;
-    }
-
-    public static double divide(double numberOne, double numberTwo) {
-        return numberOne / numberTwo;
     }
 
 }
